eb832a704e966d9b99afba2b40411c8158e93e8b
agustinduarte1 <agustindrt49@gmail.com>
2025-09-15 21:24:49 -0300

    m

--- src/api/api.js
+++ src/api/api.js
@@ -1,6 +1,6 @@
 export async function consultar () {
 
-    const consulta = await fetch("https://dragonball-api.com/api/characters?limit=58")
+    const consulta = await fetch(import.meta.env.VITE_URL);
 
     const {items} = await consulta.json();
     return items ?? []
